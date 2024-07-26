8bc2d75bf063ed6cc6400ad36961955c49bcf37c
julienbltt <julien.bltt@outlook.com>
2024-07-27 01:54:29 +0200

    V1.0: Refund version for publish in PyPI repository.

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
+from setuptools import setup, find_packages
+
 setup(
         name='cliologging', # How you named your package folder
         packages=find_packages(include=['cliologging']), # Chose the same as "name"
-        version='1.1.0', # Start with a small number and increase it with every change you make
+        version='1.0', # Start with a small number and increase it with every change you make
         license='None', # Chose a license from here: https://help.github.com/articles/licensing-a-repository
         description='Add-on to the official Python logging library.', # Give a short description about your library
         author='Julien BALDERIOTTI', # Type in your name
         author_email='julien.blt@outlook.com', # Type in your E-Mail
         url='https://github.com/julienbltt/cliologging.git', # Provide either the link to your github or to your website
-        download_url='',
+        download_url='https://github.com/julienbltt/cliologging/archive/refs/tags/v1.0.tar.gz',
         keywords=['LOGGING', 'LOG', 'LOGGER'], # Keywords that define your package best
         install_requires=[],
         classifiers=[
